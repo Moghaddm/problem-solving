cf6bb5dd388589f9be381333d2f237958f599d30
Mohammad Mahdi Moghaddam <mahdimoghaddam2005@gmail.com>
2024-05-21 11:35:05 -0600

    added lengthOfLongestSubstring method of Solution class

--- leetCodesolutions.py
+++ leetCodesolutions.py
@@ -1,4 +1,3 @@
-import functools as func
 
 class Solution:
     
@@ -7,7 +6,7 @@ class Solution:
         for num in nums:
             result += [item + [num] for item in result]
         return result
-    
+
     def addTwoNumbers(self,l1,l2):
         biggestDigit = 10 ** (len(l1) - 1)
         n1 = 0
@@ -23,6 +22,23 @@ class Solution:
             
         result = int(n1 + n2)
         return [num for num in str(result)]
-        
-print(Solution().addTwoNumbers([1,2,3],[2,3,4]))
-print(Solution().subsets([1,2,3]))
+    
+    def lengthOfLongestSubstring(self,s):
+        subs = [] 
+        for char in s:
+            charIndex=list(s).index(char)
+            charTemp =charIndex
+            for c in range(charTemp,len(s)):
+                sub = str(s[charIndex:c+1])
+                if(len(set(sub)) == len(sub)):
+                    print(sub)
+                    subs.append(sub)
+                else: break
+        return max([len(sub) for sub in subs])
+                
+solution = Solution()        
+print(solution.subsets([1,2,3]))
+print("-------------")
+print(solution.addTwoNumbers([1,2,3],[2,3,4]))
+print("-------------")
+print(solution.lengthOfLongestSubstring("pwwkew"))
